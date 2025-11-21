8df5733c517c2f64ef0a8d11b01208e4ff243bfd
fatih <rinqtf@gmail.com>
2025-11-21 19:22:10 +0300

    feat(thumbnails): save uploaded thumbnails to disk

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -3,7 +3,8 @@ import { respondWithJSON } from "./json";
 import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
-import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
+import { BadRequestError, UserForbiddenError } from "./errors";
+import path from "path";
 
 const MAX_UPLOAD_SIZE = 10 << 20;
 export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
@@ -29,9 +30,7 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   }
 
   const type = image.type;
-  const imageData = await image.arrayBuffer();
-  const imageBuffer = Buffer.from(imageData).toString("base64");
-  const dataUrl = `data:${type};base64,${imageBuffer}`;
+  const dataUrl = path.join(cfg.assetsRoot, `${videoId}.${type.split("/")[1]}`);
 
   const metadata = getVideo(cfg.db, videoId);
 
@@ -41,7 +40,9 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
     );
   }
 
-  metadata.thumbnailURL = dataUrl;
+  Bun.write(dataUrl, image);
+
+  metadata.thumbnailURL = `http://localhost:${cfg.port}/${dataUrl}`;
 
   updateVideo(cfg.db, metadata);
 
